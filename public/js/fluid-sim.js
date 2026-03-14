68bade1a2d85540d152c33ba2165664dfdb2a53f
Quinn Aho <quinnaho123@gmail.com>
2026-03-14 15:47:51 -0400

    update invert mask

--- public/js/fluid-sim.js
+++ public/js/fluid-sim.js
@@ -146,6 +146,11 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
+function uploadCollisionMaskTexture() {
+    gl.bindTexture(gl.TEXTURE_2D, textMaskTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textCanvas);
+}
+
 function updateCollisionMask() {
     textCanvas.width = gl.drawingBufferWidth;
     textCanvas.height = gl.drawingBufferHeight;
@@ -166,14 +171,12 @@ function updateCollisionMask() {
             textCtx.drawImage(img, x, y, logoSize, logoSize);
             
             // Re-upload the texture to GPU asynchronously
-            gl.bindTexture(gl.TEXTURE_2D, textMaskTexture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textCanvas);
+            uploadCollisionMaskTexture();
         };
         img.src = '/assets/images/unreal-engine.svg';
     } else {
         // Just upload the cleared blank canvas if no collision logo is needed
-        gl.bindTexture(gl.TEXTURE_2D, textMaskTexture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textCanvas);
+        uploadCollisionMaskTexture();
     }
 }
 
